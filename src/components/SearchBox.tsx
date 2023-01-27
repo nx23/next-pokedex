06e5a475156847e15f6997f6ad2f1dd505648fda
Gabriel Botareli <gbotareli@gmail.com>
2023-01-27 00:07:23 -0300

    fix: searchbox

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import styles from './SearchBox.module.css'
 import Image from 'next/image'
 import Lupa from 'public/Lupa.svg'
@@ -7,11 +7,11 @@ import Lupa from 'public/Lupa.svg'
 export function SearchBox() {
   return (
     <InputGroup className={styles.search}>
-      <InputLeftElement
-        className={styles.searchInputIcon}
-        pointerEvents='none'
-        children={<Image src={Lupa} alt='Lupa' className={styles.lupa}/>}
-      />
+        <InputLeftElement
+          className={styles.searchInputIcon}
+          pointerEvents='none'>
+            <Image src={Lupa} alt='Lupa' className={styles.lupa}/>
+        </InputLeftElement>
       <Input placeholder='Procurar' className={styles.searchInput}/>
     </InputGroup>
   )
